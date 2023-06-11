7d70202c7fbd38dfb454d4cff5c3a14476ec81e4
Jefferson Ribeiro <ribeiro.jefferson@escolar.ifrn.edu.br>
2023-06-11 11:03:36 -0300

    Implementa a funcionalidade da tela de perfil

--- app/src/main/java/com/example/nab/HomeActivity.java
+++ app/src/main/java/com/example/nab/HomeActivity.java
@@ -17,33 +17,26 @@ public class HomeActivity extends AppCompatActivity {
     private Button button2;
     private Button button3;
 
-    // Define a constant to identify the request code
     private static final int NEW_SCREEN_REQUEST_CODE = 1;
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content view to the activity_home.xml layout file
         setContentView(R.layout.activity_home);
 
-        // Initialize the TextView for displaying the username
         usernameTextView = findViewById(R.id.usernameTextView);
 
-        // Get the username passed from the MainActivity
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra("user")) {
-            User user = (User) intent.getSerializableExtra("user");
-
-            // Display the username at the top of the screen
+            user = (User) intent.getSerializableExtra("user");
             usernameTextView.setText("Bem vindo, " + user.getName() + "!");
 
-            // Initialize the Buttons
             button1 = new Button(this);
             button2 = new Button(this);
             button3 = new Button(this);
 
-            // Set text and click listeners for the Buttons
             button1.setText("JOGAR");
             button1.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -74,12 +67,10 @@ public class HomeActivity extends AppCompatActivity {
                 }
             });
 
-            // Add the Buttons to the layout
             LinearLayout buttonLayout = findViewById(R.id.buttonLayout);
             buttonLayout.addView(button1);
             buttonLayout.addView(button2);
             buttonLayout.addView(button3);
-
         }
     }
 
@@ -89,10 +80,9 @@ public class HomeActivity extends AppCompatActivity {
 
         if (requestCode == NEW_SCREEN_REQUEST_CODE && resultCode == RESULT_OK) {
             if (data != null && data.hasExtra("user")) {
-                User user = (User) data.getSerializableExtra("user");
-                // Use the updated user object here
+                user = (User) data.getSerializableExtra("user");
+                usernameTextView.setText("Bem vindo, " + user.getName() + "!");
             }
         }
     }
-
 }
--- app/src/main/java/com/example/nab/PerfilActivity.java
+++ app/src/main/java/com/example/nab/PerfilActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class PerfilActivity extends AppCompatActivity {
 
     private User user;
+    private EditText nameEditText;
+    private EditText emailEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +26,51 @@ public class PerfilActivity extends AppCompatActivity {
             this.user = (User) intent.getSerializableExtra("user");
         }
 
-        // Create a button dynamically
+        // Create text fields for changing user info
+        nameEditText = new EditText(this);
+        nameEditText.setText(user.getName());
+        emailEditText = new EditText(this);
+        emailEditText.setText(user.getEmail());
+
+        // Create the back button
         Button backButton = new Button(this);
         backButton.setText("Voltar");
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams backButtonParams = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
         );
-        params.setMargins(0, 16, 0, 0);
-        backButton.setLayoutParams(params);
+        backButtonParams.setMargins(0, 16, 0, 0);
+        backButton.setLayoutParams(backButtonParams);
 
-        // Set click listener for the button
+        // Set click listener for the back button
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Return to the HomeActivity
+                // Return to the HomeActivity without saving changes
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+
+        // Create the save button
+        Button saveButton = new Button(this);
+        saveButton.setText("Salvar");
+        LinearLayout.LayoutParams saveButtonParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        saveButtonParams.setMargins(0, 16, 0, 0);
+        saveButton.setLayoutParams(saveButtonParams);
+
+        // Set click listener for the save button
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Update the user information
+                user.setName(nameEditText.getText().toString());
+                user.setEmail(emailEditText.getText().toString());
+
+                // Return to the HomeActivity with the updated user
                 Intent returnIntent = new Intent();
                 returnIntent.putExtra("user", user);
                 setResult(RESULT_OK, returnIntent);
@@ -46,8 +78,19 @@ public class PerfilActivity extends AppCompatActivity {
             }
         });
 
-        // Add the button to the layout
+        // Add the text fields and buttons to the layout
         LinearLayout layout = findViewById(R.id.perfilLayout);
-        layout.addView(backButton);
+        LinearLayout pfb = findViewById(R.id.profileButtonLayout);
+        layout.addView(nameEditText);
+        layout.addView(emailEditText);
+        pfb.addView(backButton);
+        pfb.addView(saveButton);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Return to the HomeActivity without saving changes when the back button is pressed
+        setResult(RESULT_CANCELED);
+        super.onBackPressed();
     }
 }
